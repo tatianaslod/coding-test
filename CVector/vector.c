6e088386bcab1388863cc39b8538a0dea83e9bce
carola <carola@Fabricio.local>
2015-06-07 23:14:20 -0300

    Some CR comments.

--- CVector/vector.c
+++ CVector/vector.c
@@ -1,5 +1,7 @@
 #include "vector.h"
 
+// F: try to always indent with spaces rather than a mix with tabs and spaces. Regular code formating can be 2 spaces, or maybe 4 spaces.
+
 double getLoad(Vector* vector);
 
 Vector* create_vector(int initial_size){
@@ -30,12 +32,15 @@ int get(Vector* vector, int i){
 
 bool set(Vector* vector, int i, int x){
 	if (((*vector).size + 1) >= (*vector).max_size) {
+		// F: Aca es donde agrandas el vector. En realidad se deberia agrandar cuando el load factor es mayor al 90%. El caso donde size+1 >= max_size esta incluido en el otro
+
 	    return false;
 	}
 	//load of array
 	
 	if (getLoad(vector) > 0.9) { //it works without the if :(
 		// Vector *newVector = create_vector((*vector).size * 2);
+        // F: do you really want to create a new Vector? Or maybe just instantiate a new data array?
 		// if (newVector == NULL) {
 		// 	return false;
 		// }
@@ -43,6 +48,7 @@ bool set(Vector* vector, int i, int x){
 		// free(vector);
 		// vector = newVector; 
 	}
+    // F: what is this for doing? What's for? Is it swaping all the values? the set function should be O(1).
     for (int j = (*vector).size - 1; j > i + 1; j--) {
 		int tmp = (*vector).data[j];
 		(*vector).data[j] = (*vector).data[j-1];
@@ -53,6 +59,8 @@ bool set(Vector* vector, int i, int x){
 }
 
 double getLoad(Vector* vector) {
+    // F: Should be:
+    // return size * 1.0 / max_size;  // La multiplicacion por 1.0 es para convertir 'size' a double.
 	int elements_with_value_count = 0;
 	for (int j = 0; j < (*vector).size; j++) {
 		if ((*vector).data[j] != 0) {
