c86cc24a4d19f0c92c53a358840f464106cbde18
Tatiana Slod <tatianaslod@gmail.com>
2015-06-14 19:26:43 -0300

    Add useful templates.

---
+++ CrackingCodeInterview/template.c
@@ -0,0 +1,8 @@
+//rm -f a.out && gcc template.c && ./a.out
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello\n");
+    return 0;
+}
---
+++ CrackingCodeInterview/template.cc
@@ -0,0 +1,8 @@
+//rm -f a.out && g++ template.cc && ./a.out
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
